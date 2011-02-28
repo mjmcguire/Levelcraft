e162eb5e508ef519bb278543dd125bb917281cd5
Samkio <supercool_monkey@hotmail.co.uk>
2011-02-28 15:25:52 +0000

    Fixed Whitelist

--- src/me/samkio/levelcraft/Whitelist.java
+++ src/me/samkio/levelcraft/Whitelist.java
@@ -4,26 +4,20 @@ package me.samkio.levelcraft;
 public class Whitelist {
 
 	public static boolean isAdmin(String player) {
-		boolean Admin = false;
 		for (int i = 0; i < Settings.LCAdmins.length; i++) {
 			if (Settings.LCAdmins[i].equalsIgnoreCase(player)) {
-				Admin = true;
-			} else {
-				Admin = false;
-			}
+				return true;
+			} 
 		}
-		return Admin;
+		return false;
 	}
 
 	public static boolean isAvoid(String player) {
-		boolean Avoid = false;
 		for (int i = 0; i < Settings.LCAvoiders.length; i++) {
 			if (Settings.LCAvoiders[i].equalsIgnoreCase(player)) {
-				Avoid = true;
-			} else {
-				Avoid = false;
+				return true;
 			}
 		}
-		return Avoid;
+		return false;
 	}
 }
